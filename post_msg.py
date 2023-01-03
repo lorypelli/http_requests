198f49cc0b6aca38aeca934ae0e990861d57d622
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-01-03 22:39:10 +0100

    Take the values from config.py if not null

--- post_msg.py
+++ post_msg.py
@@ -4,18 +4,24 @@ import os #importo la libreria os per controllare se esiste la cartella
 import config #importo il file config.py (puoi fare semplicemente invio nell'input e userà i valori del file di configurazione)
 if (os.path.exists("./__pycache__")): #se esiste
     shutil.rmtree("./__pycache__") #rimuovo la cartella __pycache__ perchè è inutile
-bot_token = input("Insert the bot token ") or config.bot_token  #ricevo il token del bot in input
-response = requests.get("https://discord.com/api/auth/login", headers = { #controllo se il token ricevuto in input è valido
+if (config.bot_token != None): #se il token nel file config.py non è nullo
+    bot_token = config.bot_token #allora quello sarà il token
+else :
+    bot_token = input("Insert the bot token ") #altrimenti lo ricevo in input
+response = requests.get("https://discord.com/api/auth/login", headers = { #controllo se il token è valido
     "authorization": "Bot " + bot_token
 })
 while (response.status_code != 200): #fino a quando è valido lo continuo a chiedere e controllo se quello inserito è valido
     print("There was an error, the bot token isn't valid. Try again!")
-    bot_token = input("Insert the bot token ") or config.bot_token
+    bot_token = input("Insert the bot token ")
     response = requests.get("https://discord.com/api/auth/login", headers = {
     "authorization": "Bot " + bot_token
 })
-channel_id = input("Insert the channel id ") or config.channel_id #ricevo l'id del canale in input
-response = requests.get("https://discord.com/api/channels/" + channel_id, headers = { #controllo se l'id ricevuto in input è valido
+if (config.channel_id != None): #se l'id nel file config.py non è nullo
+    channel_id = config.channel_id #allora quello sarà l'id
+else :
+    channel_id = input("Insert the channel id ") #altrimenti lo ricevo in input
+response = requests.get("https://discord.com/api/channels/" + channel_id, headers = { #controllo se l'id è valido
     "authorization": "Bot " + bot_token
 })
 while (response.status_code != 200): #fino a quando è valido lo continuo a chiedere e controllo se quello inserito è valido
