3a9bbdf534d239647a86c06d8118c6c758e0acff
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-01-04 14:21:51 +0100

    Check if config.py values are valid

--- post_msg.py
+++ post_msg.py
@@ -4,7 +4,10 @@ from os import path #I use this to check if the folder exists
 from config import bot_token, channel_id #Values will be used if not null
 if (path.exists("./__pycache__")): #if exists
     rmtree("./__pycache__") #delete the __pycache__ folder because it's unuseful for my project
-if (bot_token != ""): #if the token in the config.py file is not null
+response = get("https://discord.com/api/auth/login", headers = { #check if the token is valid
+    "authorization": "Bot " + bot_token
+})
+if (response.status_code == 200): #if the token in the config.py file is valid
     bot_token = bot_token #it will be the token
     print("The bot token has been taken from the config.py file")
 else :
@@ -18,7 +21,10 @@ while (response.status_code != 200): #until is valid it asks for a new token and
     response = get("https://discord.com/api/auth/login", headers = {
     "authorization": "Bot " + bot_token
 })
-if (channel_id != ""): #if the id in the config.py file is not null
+response = get("https://discord.com/api/channels/" + channel_id, headers = {
+    "authorization": "Bot " + bot_token
+})
+if (response.status_code == 200): #if the id in the config.py file is valid
     channel_id = channel_id #it will be the id
     print("The channel id has been taken from the config.py file")
 else :
