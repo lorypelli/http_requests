919f596134e8b810a4e77e7a77ccd396e129167a
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-01-05 19:26:43 +0100

    BIG UPDATE!!! Now with a GUI and an icon

--- http_requests.py
+++ http_requests.py
@@ -1,69 +1,95 @@
-from requests import get, post, delete #the library requests it's used to make POST, GET and DELETE requests
-from pick import pick
-from shutil import rmtree #I use this to remove a folder
-from os import path #I use this to check if the folder exists
-from config import bot_token, channel_id #these values will be used if not null
-if (path.exists("./__pycache__")): #if exists
-    rmtree("./__pycache__") #delete the __pycache__ folder because it's unuseful for my project
-response = get("https://discord.com/api/auth/login", headers = { #check if the token is valid
-    "authorization": "Bot " + bot_token
-})
-if (response.status_code == 200): #if the token in the config.py file is valid
-    bot_token = bot_token #it will be the token
-    print("The bot token has been taken from the config.py file")
-else :
-    bot_token = input("Insert the bot token ") #else the program will receive it from input
-response = get("https://discord.com/api/auth/login", headers = { #check if the token is valid
-    "authorization": "Bot " + bot_token
-})
-while (response.status_code != 200): #until is valid it asks for a new token and check again
-    print("There was an error, try again!")
-    bot_token = input("Insert the bot token ")
-    response = get("https://discord.com/api/auth/login", headers = {
-    "authorization": "Bot " + bot_token
-})
-response = get("https://discord.com/api/channels/" + channel_id, headers = {
-    "authorization": "Bot " + bot_token
-})
-if (response.status_code == 200): #if the id in the config.py file is valid
-    channel_id = channel_id #it will be the id
-    print("The channel id has been taken from the config.py file")
-else :
-    channel_id = input("Insert the channel id ") #else the program will receive it from input
-response = get("https://discord.com/api/channels/" + channel_id, headers = { #check if the id is valid
-    "authorization": "Bot " + bot_token
-})
-while (response.status_code != 200): #until is valid it asks for a new id and check again
-    print("There was an error, try again!")
-    channel_id = input("Insert the channel id ")
-    response = get("https://discord.com/api/channels/" + channel_id, headers = {
-    "authorization": "Bot " + bot_token
-})
-menu_title = "Do you want to write a new message or delete an existing channel? (J / K to move)" #the menu title
-menu_options = ["Write a message", "Delete a channel (this will use channel id you already provided)"] #the menu options
-option, index = pick(menu_options, menu_title, indicator="–>") #pick an option
-print(option) #write the option
-if (index == 1): #if option index it's 1 (not 2 because an array starts with 0)
-    def delete_chn(chn_id: str): #use a function because is better
-        response = delete("https://discord.com/api/channels/" + chn_id, headers = { #make a request with input params
-            "authorization": "Bot " + bot_token
-        })
-        if (response.status_code != 200): #if it fails
-            print(response.status_code)
-            print("There was an error, try again!") #write that there was an error
-        else :
-            print("The channel has been deleted successfully!") #else write that the channel has been deleted successfully
-    delete_chn(channel_id) #run the function with the requested param
-else: #else option it's equal to 0
-    message = input("Insert the message ") #asks to the user for a message input
-    def post_msg(msg: str): #use a function because is better
-        response = post("https://discord.com/api/channels/" + channel_id + "/messages", headers = { #make a request with input params
-            "authorization": "Bot " + bot_token
-        }, data = {
-            "content": msg
-        })
-        if (response.status_code != 200): #if it fails
-            print("There was an error, try again!") #write that there was an error
-        else :
-            print("The message has been sent successfully!") #else write that the message has been sent successfully
-    post_msg(message) #run the function with the requested param
+import PySimpleGUI
+from requests import get, post, delete
+from shutil import rmtree
+from os import path
+import config
+if (path.exists("./__pycache__")):
+    rmtree("./__pycache__")
+PySimpleGUI.theme("BlueMono")
+import_from_config = [
+    [PySimpleGUI.Button("Import from config")]
+]
+bot_token = [
+    [PySimpleGUI.Text("Insert bot token"), PySimpleGUI.InputText(size=(100), key="tkn_textbox")],
+    [PySimpleGUI.Button("Validate")]
+]
+channel_id = [
+    [PySimpleGUI.Text("Insert channel id"), PySimpleGUI.InputText(size=(100), key="chn_textbox")],
+    [PySimpleGUI.Button("Validate")]
+]
+message = [
+    [PySimpleGUI.Text("Insert message", key="msg_text"), PySimpleGUI.Multiline(size=(100, 5), key="msg_textbox")]
+]
+send_delete_btn = [
+    [PySimpleGUI.Button("Send", key="Send_Delete")]
+]
+list = [
+    [PySimpleGUI.Text("Select action"), PySimpleGUI.Combo(["Write a message", "Delete a channel"], size=(100), default_value="Write a message", readonly=True, key="selectbox"), PySimpleGUI.Button("Confirm")]
+]
+layout = [
+    [import_from_config, bot_token, channel_id, list, message, send_delete_btn]
+]
+window = PySimpleGUI.Window("http_requests", layout, element_justification="c", icon="./app_icon.ico")
+while True:
+    event, values = window.read()
+    if (event == "Import from config"):
+        response = get("https://discord.com/api/auth/login", headers = {
+            "authorization": "Bot " + config.bot_token
+    })
+        if (response.status_code == 200):
+            window["tkn_textbox"].Update(config.bot_token)
+        response = get("https://discord.com/api/channels/" + config.channel_id, headers = {
+            "authorization": "Bot " + config.bot_token
+    })
+        if (response.status_code == 200):
+            window["chn_textbox"].Update(config.channel_id)
+    if (event == "Validate"):
+        response = get("https://discord.com/api/auth/login", headers = {
+            "authorization": "Bot " + values["tkn_textbox"]
+    })
+        if (response.status_code != 200 and event == "Validate"):
+            PySimpleGUI.popup("There was an error, try again!", no_titlebar=True)
+        elif (response.status_code == 200 and event == "Validate"):
+            PySimpleGUI.popup("Validation Passed!", no_titlebar=True)
+    elif (event == "Validate0"):
+        response = get("https://discord.com/api/channels/" + values["chn_textbox"], headers = {
+            "authorization": "Bot " + values["tkn_textbox"]
+    })
+        if (response.status_code != 200 and event == "Validate0"):
+            PySimpleGUI.popup("There was an error, try again!", no_titlebar=True)
+        elif (response.status_code == 200 and event == "Validate0"):
+            PySimpleGUI.popup("Validation Passed!", no_titlebar=True)
+    elif (event == PySimpleGUI.WIN_CLOSED):
+        break
+    elif (values["selectbox"] == "Delete a channel"):
+        window["msg_text"].Update(visible = False)
+        window["msg_textbox"].Update(visible = False)
+        window["Send_Delete"].Update("Delete")
+    else:
+        window["msg_text"].Update(visible = True)
+        window["msg_textbox"].Update(visible = True)
+        window["Send_Delete"].Update("Send")
+    if (values["selectbox"] != "Delete a channel"):
+        def post_msg(msg: str):
+            response = post("https://discord.com/api/channels/" + values["chn_textbox"] + "/messages", headers = {
+                "authorization": "Bot " + values["tkn_textbox"]
+            }, data = {
+                "content": msg
+            })
+            if (response.status_code != 200 and event == "Send_Delete"):
+                PySimpleGUI.popup("There was an error, try again!", no_titlebar=True)
+            elif (response.status_code == 200 and event == "Send_Delete"):
+                PySimpleGUI.popup("The message has been sent successfully!", no_titlebar=True)
+        if (event == "Send_Delete"):
+            post_msg(values["msg_textbox"])
+    elif (values["selectbox"] == "Delete a channel"):
+        def delete_chn(chn_id: str):
+            response = delete("https://discord.com/api/channels/" + chn_id, headers = {
+                "authorization": "Bot " + values["tkn_textbox"]
+            })
+            if (response.status_code != 200 and event == "Send_Delete"):
+                PySimpleGUI.popup("There was an error, try again!", no_titlebar=True)
+            elif (response.status_code == 200 and event == "Send_Delete"):
+                PySimpleGUI.popup("The channel has been deleted successfully!", no_titlebar=True)
+        if (event == "Send_Delete"):
+            delete_chn(values["chn_textbox"])
